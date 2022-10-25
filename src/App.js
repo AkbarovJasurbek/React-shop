0a1cfe337e41c06ee6b98f254e1e4cc5bedb092b
Akbarov Jasurbek <akbarovjasurbek28@gmail.com>
2022-10-25 18:48:11 +0900

    Bissmillah

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
+import {Shop} from "./components/Shop";
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Shop/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API_URL = "https://fortniteapi.io/v1/shop?lang=en";
+const API_KEY = "6bd7c162-148dc95e-191d925a-52b12d31"
+
+
+export {API_KEY, API_URL};
